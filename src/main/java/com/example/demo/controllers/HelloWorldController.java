4cfa46f22b069130e42c0ad14467beeeadfc7376
LineNicacio <line.nicacio@hotmail.com>
2023-10-23 18:46:39 -0300

    fix: endpoints adjustments

--- src/main/java/com/example/demo/controllers/HelloWorldController.java
+++ src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -1,24 +1,22 @@
 package com.example.demo.controllers;
 
+import com.example.demo.domain.Hello;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/hello")
 @Tag(name = "Hello example", description = "Rest api hello world")
 public class HelloWorldController {
 
-    @GetMapping
-    public ResponseEntity<String> getExample() {
-        return ResponseEntity.ok("Hello");
+    @GetMapping("/{name}")
+    public ResponseEntity<String> getExample(@PathVariable String name) {
+        return ResponseEntity.ok("Hello "+ name);
     }
 
-    @PostMapping("/{text}")
-    public ResponseEntity<String> postExample(String text) {
-        return ResponseEntity.ok(text);
+    @PostMapping
+    public ResponseEntity<Hello> postExample(@RequestBody Hello hello) {
+        return ResponseEntity.ok().body(hello);
     }
 }
